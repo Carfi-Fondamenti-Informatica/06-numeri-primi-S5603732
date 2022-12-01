2875e759d4b7bac32e7633298c516cf24c1c7b68
S5603732 <118397147+S5603732@users.noreply.github.com>
2022-12-01 17:44:02 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "primalibreria.h"
+
+int numeroprimo (int a, int b) {
+    if (a%b!=0) {
+        b--;
+        if (b>1){
+            numeroprimo(a,b);
+        }
+        if(b==2) return 1;
+    }
+    else return 0;
+}
 
