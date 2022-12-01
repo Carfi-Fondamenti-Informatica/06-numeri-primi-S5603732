6e7ff1ecdf1ba8b6d0ee4d46350372a6ccfa2363
S5603732 <118397147+S5603732@users.noreply.github.com>
2022-12-01 18:52:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,17 +4,11 @@
 int main(){
  int a, b;
     cin >> a;
-    b=a-1;
-    if(a==4) {
-        cout<<"numero non primo";
-        return 0;
+    b = a - 1;
+    if (numeroprimo (a,b) == true) {
+        cout << "numero primo";
+    } else if (numeroprimo (a,b) == false) {
+        cout << "numero non primo";
     }
-    if(a==1||a==2||a==3) {
-        cout<<"numero primo";
-        return 0;
-    }
-    if (numeroprimo(a,b)==1) cout<<"numero primo";
-    else cout<<"numero non primo";
-
     return 0;
 }
