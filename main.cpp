7f4cec26f21a93b823cbcef0f4ef041bae0cb871
S5603732 <118397147+S5603732@users.noreply.github.com>
2022-12-01 18:04:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,19 @@
 #include "primalibreria.h"
 
 int main(){
-  int a, b;
+ int a, b;
     cin >> a;
     b=a-1;
     if(a==4) {
-        cout<<"Falso";
+        cout<<"numero non primo";
         return 0;
     }
     if(a==1||a==2||a==3) {
-        cout<<"Vero";
+        cout<<"numero primo";
         return 0;
     }
-    if (numeroprimo(a,b)==1) cout<<"Vero";
-    else cout<<"Falso";
-  return 0;
+    if (numeroprimo(a,b)==1) cout<<"numero primo";
+    else cout<<"numero non primo";
+
+    return 0;
 }
